7d017cf4ad7ec7f013eb7bc7826ee3a174a4e126
Usuario <Usuario@DESKTOP-31Q3HS2>
2025-04-10 17:05:51 -0500

    Agregar archivos de proyecto.

---
+++ QuispeRLigaPro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuispeRLigaPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
