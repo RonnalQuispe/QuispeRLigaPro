d524bc0118196ccdef3b776ac437608316fc2983
QuispeR <ronny.quispe@udla.edu.ec>
2025-04-15 18:18:57 -0500

    generamos una accion en el boton Editar

--- QuispeRLigaPro/Controllers/EquipoController.cs
+++ QuispeRLigaPro/Controllers/EquipoController.cs
@@ -1,6 +1,7 @@
 ﻿using QuispeRLigaPro.Models;
 using Microsoft.AspNetCore.Mvc;
 using QuispeRLigaPro.Repositories;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace QuispeRLigaPro.Controllers
 {
@@ -8,10 +9,23 @@ namespace QuispeRLigaPro.Controllers
     {
         public IActionResult List()
         {
-         EquipoRepository repositorio = new EquipoRepository();
-            var equipos= repositorio.DevuelveListadoEquipos();
+            EquipoRepository repositorio = new EquipoRepository();
+            var equipos = repositorio.DevuelveListadoEquipos();
             return View(equipos);
         }
+        public IActionResult EditarEquipo()
+        {
+            Equipo ldu = new Equipo { 
+            Id = 1,
+                Nombre = "LDU",
+                PartidosJugados = 10,
+                PartidosGanados = 10,
+                PartidosEmpatados = 0,
+                PartidosPerdidos = 0,
+                TotalPuntos = 30,
+                };
+            return View(ldu);
+    }
     }
 }
 
